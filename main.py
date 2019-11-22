cdda5a63f447d3007f1f41e1af087d0e67f479b8
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2019-11-22 16:55:06 -0300

    Started implementing 'list' command

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import requests
 from hashlib import sha256
 import argparse
 import shlex
+from minor_gaugi import Color
 
 class ZeusCLI(Cmd):
 
@@ -112,6 +113,56 @@ Type '?' for a list of commands
     except requests.exceptions.ConnectionError:
       self.msg_error ("Failed to connect to LPS Cluster.")
     print ()
+
+  #
+  # Lists all tasks
+  #
+  def do_list (self, inp):
+
+    # Requesting task list for the cluster
+    try:
+      r = requests.get(url='http://localhost:5020/tasks')
+      print (r.text)
+      # # define the line template
+      # line="+------------------+----------------------------------------------------------------------------------+----------+----------+----------+----------+----------+------------+"
+      # print(line)
+      # print ( "|     "+Color.CGREEN2+"username"+Color.CEND+
+      # "     |                                     "+Color.CGREEN2+"taskname"+Color.CEND+
+      # "                                     | "+Color.CGREEN2+"assigned"+Color.CEND+
+      # " | "+Color.CGREEN2+"testing"+Color.CEND+
+      # "  | "+Color.CGREEN2+"running"+Color.CEND+
+      # "  | "+Color.CRED2+"failed"+Color.CEND+
+      # "   |  "+Color.CGREEN2+"done"+Color.CEND+
+      # "    | "+Color.CGREEN2+"status"+Color.CEND+"     |" )
+      # print(line)
+      # for task in tasks:
+      #   if len(task.taskName)>80:
+      #     #taskname = task.taskName[0:75]
+      #     taskname = task.taskName[0:40]+' ... '+ task.taskName[-30:]
+      #   else:
+      #     taskname = task.taskName
+      #   print ( ("| {0:<16} | {1:<80} | {2:<8} | {3:<8} | {4:<8} | {5:<8} | {6:<8} | {7:<15}"+Color.CEND+" |" ).format( task.username, taskname, task.assigned, task.testing,
+      #       task.running, task.failed, task.done, getStatus(task.status)))
+      # print(line)
+    except:
+      self.msg_error ("Failed to connect to LPS Cluster.")
+
+    def getStatus(status):
+      if status == 'registered':
+        return Color.CWHITE2+"REGISTERED"
+      elif status == 'assigned':
+        return Color.CWHITE2+"ASSIGNED"
+      elif status == 'testing':
+        return Color.CGREEN2+"TESTING"
+      elif status == 'running':
+        return Color.CGREEN2+"RUNNING"
+      elif status == 'done':
+        return Color.CGREEN2+"DONE"
+      elif status == 'failed':
+        return Color.CRED2+"FAILED"
+      elif status == 'finalized':
+        return Color.CRED2+"FINALIZED"
+
 
   #
   # Create task
