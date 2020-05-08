e2e9581d25a5d7257c4abb9247b3983c1530abda
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2020-05-08 15:55:58 -0300

    Fix on version checking

--- lps_maestro/__init__.py
+++ lps_maestro/__init__.py
@@ -22,31 +22,28 @@ from . import utils
 __all__.extend(utils.__all__)
 from .utils import *
 
-# Extracted from https://stackoverflow.com/questions/58648739/how-to-check-if-python-package-is-latest-version-programmatically
-import subprocess
-import sys
+# Checking for new versions
+import pkg_resources
+import requests
 def check(name):
-    latest_version = str(subprocess.run([sys.executable, '-m', 'pip', 'install', '{}==random'.format(name)], capture_output=True, text=True))
-    latest_version = latest_version[latest_version.find('(from versions:')+15:]
-    latest_version = latest_version[:latest_version.find(')')]
-    latest_version = latest_version.replace(' ','').split(',')[-1]
-
-    current_version = str(subprocess.run([sys.executable, '-m', 'pip', 'show', '{}'.format(name)], capture_output=True, text=True))
-    current_version = current_version[current_version.find('Version:')+8:]
-    current_version = current_version[:current_version.find('\\n')].replace(' ','') 
-
+    # Getting current version
+    current_version = pkg_resources.get_distribution('lps_maestro').version
+    # Getting latest version
+    r = requests.get('https://pypi.org/pypi/lps-maestro/json')
+    d = r.json()
+    latest_version = list(d['releases'].keys())[-1]
     if latest_version == current_version:
-        return True, latest_version
+        return True, latest_version, current_version
     else:
-        return False, latest_version
+        return False, latest_version, current_version
 
 print (" Welcome to LPS Maestro!")
 print (" - Checking for updates...")
 try:
     v = check('lps-maestro')
     if (v[0] == True):
-        print (" - This is the latest version ({})".format(v[1]))
+        print (" - This is the latest version available! (current={}, latest={})".format(v[1], v[2]))
     else:
-        print (" => Version {} is available, please update!".format(v[1]))
+        print (" => Version {} is available, please update over your {}!".format(v[1], v[2]))
 except:
     print (" - Failed to check for updates")
